f700dc8b11cec107db1f1ea8dfd6828a1f58d7f2
evagolabek <golabek.eva@gmail.com>
2018-12-09 20:26:55 +0100

    different ways for selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+   // different ways of selector
+  // 1 selector: '[app-servers]',
+  // 2 selector:'.app-servers
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
