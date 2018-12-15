02f4393a8552bca0c2e6b0e606d9cc1cce8c167c
evagolabek <golabek.eva@gmail.com>
2018-12-15 18:27:54 +0100

    1st task finished

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -5,7 +5,13 @@ import { Component } from '@angular/core';
   selector: 'app-warning-alert',
   template: `
   <p> this is a warning, you are in danger </p>
-  `
+  `,
+  styles: [
+    `p{
+      padding: 20px;
+      background-color: red;
+    }`
+  ]
 })
 export class WarningAlertComponent {
 
