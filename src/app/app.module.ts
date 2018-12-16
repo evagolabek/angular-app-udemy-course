072ceb81ea0ee3c9b15397bff09a6d31363d4f8a
evagolabek <golabek.eva@gmail.com>
2018-12-16 18:27:14 +0100

    databinding assigment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { UsernameComponent } from './username/username.component';
 
 
 
@@ -16,7 +17,8 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     ServerComponent,
     ServersComponent,
     SuccessAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
