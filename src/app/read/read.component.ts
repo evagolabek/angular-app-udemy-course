d3b6985cab1b7e7d89ca0d1fc3e38841bda87f23
evagolabek <golabek.eva@gmail.com>
2018-12-22 17:03:54 +0100

    read component defining observable

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+import { AppState } from './../app.state';
+import { Observable } from 'rxjs';
+
+
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
@@ -7,7 +13,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadComponent implements OnInit {
 
-  constructor() { }
+  //section 1
+  tutorials: Observable<Tutorial[]>;
+
+  //section 2
+
+  constructor(private store: Store<AppState>) {
+    this.tutorials = store.select('tutorial');
+   }
 
   ngOnInit() {
   }
